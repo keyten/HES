1f1f472724728996f544b22cc1f1289bfbde9868
Anton Konovalov <extempl@gmail.com>
2016-03-09 17:37:38 +0200

    Reformat code style.

--- hes.user.js
+++ hes.user.js
@@ -22,7 +22,7 @@
 
 // Настройки
 // Авторы, посты которых скрываем
-(function(window, $, undefined){
+(function (window, $, undefined) {
 	var config = {
 		hidePosts: {
 			enabled: true,
@@ -32,7 +32,7 @@
 				'ivansychev',
 				'ragequit',
 				'SLY_G'
-		 ],
+			],
 			mode: 'hideContent'
 		},
 
@@ -45,10 +45,10 @@
 
 	// подгружаем настройки из localStorage
 	var citem;
-	if( citem = localStorage.getItem('us_config') ){
+	if (citem = localStorage.getItem('us_config')) {
 		citem = JSON.parse(citem);
-		for(var key in citem){
-			if(!Object.prototype.hasOwnProperty.call(citem, key))
+		for (var key in citem) {
+			if (!Object.prototype.hasOwnProperty.call(citem, key))
 				continue;
 			config[key] = citem[key];
 		}
@@ -57,17 +57,17 @@
 		localStorage.setItem('us_config', JSON.stringify(config));
 	}
 
-	$(function(){
+	$(function () {
 
 		// скрываем авторов
-		if(config.hidePosts.enabled
-			 && config.hidePosts.authors.length > 0){
-			$('.post').each(function(){
-				var author = trim( $('.post-author__link', this).text() );
+		if (config.hidePosts.enabled
+			&& config.hidePosts.authors.length > 0) {
+			$('.post').each(function () {
+				var author = trim($('.post-author__link', this).text());
 				author = author.substr(1);
 				console.log(author);
-				if( config.hidePosts.authors.indexOf( author ) > -1 ){
-					if( config.hidePosts.mode == 'hideContent')
+				if (config.hidePosts.authors.indexOf(author) > -1) {
+					if (config.hidePosts.mode == 'hideContent')
 						$('.hubs, .content', this).hide();
 					else
 						$(this).hide();
@@ -76,10 +76,10 @@
 		}
 
 		// красивые формулы
-		if(config.mathjax){
+		if (config.mathjax) {
 			var id = 0;
 			// заменяем картинки на формулы
-			$('img[src^="https://tex.s2cms.ru/svg/"]').each(function(){
+			$('img[src^="https://tex.s2cms.ru/svg/"]').each(function () {
 
 				var $this = $(this);
 
@@ -92,10 +92,10 @@
 				var span = $('<span></span>');
 
 				// проверяем, использовать $ или $$
-				if(!this.id)
+				if (!this.id)
 					this.id = 'texImage' + (id++);
 
-				if( $('div[style="text-align:center;"] > #' + this.id).length > 0 ){
+				if ($('div[style="text-align:center;"] > #' + this.id).length > 0) {
 					span.text('$$tex' + code + '$$');
 				}
 				else {
@@ -120,7 +120,7 @@
 		}
 
 		// night mode
-		if(config.nightMode){
+		if (config.nightMode) {
 			var s = document.createElement('style');
 			s.id = 'us_nmstyle';
 			$.get('https://raw.githubusercontent.com/WaveCutz/habrahabr.ru_night-mode/master/userstyle.css').then(function (data) {
@@ -129,10 +129,10 @@
 			});
 		}
 
-        // скрываем плашки с именем-кармой-etc юзера
-        if(config.hideUserInfo){
-            $('*[rel=user-popover]').webuiPopover('destroy');
-        }
+		// скрываем плашки с именем-кармой-etc юзера
+		if (config.hideUserInfo) {
+			$('*[rel=user-popover]').webuiPopover('destroy');
+		}
 
 		// добавляем менюшку с конфигом
 		{
@@ -142,19 +142,19 @@
 			$tab.append($menu);
 
 			var $he_button = $('<a href="javascript://"></a>');
-			if(!config.hidePosts.enabled)
+			if (!config.hidePosts.enabled)
 				$he_button.text('Скрывать посты: off');
-			else if(config.hidePosts.mode == 'hideContent')
+			else if (config.hidePosts.mode == 'hideContent')
 				$he_button.text('Скрывать частично');
 			else
 				$he_button.text('Скрывать посты: on');
-			$he_button.click(function(){
-				if(!config.hidePosts.enabled){
+			$he_button.click(function () {
+				if (!config.hidePosts.enabled) {
 					config.hidePosts.enabled = true;
 					config.hidePosts.mode = 'hide';
 					$he_button.text('Скрывать посты: on');
 				}
-				else if(config.hidePosts.mode == 'hideContent'){
+				else if (config.hidePosts.mode == 'hideContent') {
 					config.hidePosts.enabled = false;
 					$he_button.text('Скрывать посты: off');
 				}
@@ -168,9 +168,9 @@
 			$he_button.appendTo($menu);
 
 			var $ha_button = $('<a href="javascript://">Скрываемые авторы</a>');
-			$ha_button.click(function(){
+			$ha_button.click(function () {
 				var auth = window.prompt('Через запятую (можно пробелы), регистр важен', config.hidePosts.authors.join(', '));
-				if(!auth)
+				if (!auth)
 					return;
 				config.hidePosts.authors = auth.replace(/\s/g, '').split(',');
 				localStorage.setItem('us_config', JSON.stringify(config));
@@ -178,8 +178,8 @@
 			$ha_button.appendTo($menu);
 
 			var $mj_button = $('<a href="javascript://">MathJax: ' + (config.mathjax ? 'on' : 'off') + '</a>');
-			$mj_button.click(function(){
-				if(config.mathjax){
+			$mj_button.click(function () {
+				if (config.mathjax) {
 					config.mathjax = false;
 					$mj_button.text('MathJax: off');
 				}
@@ -188,16 +188,16 @@
 					$mj_button.text('MathJax: on');
 				}
 				localStorage.setItem('us_config', JSON.stringify(config));
-		 });
+			});
 			$mj_button.appendTo($menu);
 
 			var $nm_button = $('<a href="javascript://">Night mode: ' + (config.nightMode ? 'on' : 'off') + '</a>');
-			$nm_button.click(function(){
-				if(config.nightMode){
+			$nm_button.click(function () {
+				if (config.nightMode) {
 					config.nightMode = false;
 					$nm_button.text('Night mode: off');
 					var s = document.getElementById('us_nmstyle');
-					if(s)
+					if (s)
 						document.head.removeChild(s);
 				}
 				else {
@@ -214,10 +214,10 @@
 			});
 			$nm_button.appendTo($menu);
 
-            // Hide user info. Аббревиатуру не делать.
-            var $h_button = $('<a href="javascript://">Скрывать юзеринфо: ' + (config.hideUserInfo ? 'on' : 'off') + '</a>');
-			$h_button.click(function(){
-				if(config.hideUserInfo){
+			// Hide user info. Аббревиатуру не делать.
+			var $h_button = $('<a href="javascript://">Скрывать юзеринфо: ' + (config.hideUserInfo ? 'on' : 'off') + '</a>');
+			$h_button.click(function () {
+				if (config.hideUserInfo) {
 					config.hideUserInfo = false;
 					$h_button.text('Скрывать юзеринфо: off');
 				}
@@ -226,13 +226,13 @@
 					$h_button.text('Скрывать юзеринфо: on');
 				}
 				localStorage.setItem('us_config', JSON.stringify(config));
-		    });
+			});
 			$h_button.appendTo($menu);
 
 		}
 	});
 
-	function trim(str){
+	function trim(str) {
 		return str.replace(/^\s*/, '').replace(/\s*$/, '');
 	}
 })(window, window.jQuery);
